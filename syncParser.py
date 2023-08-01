2bb3c6e1ee69421d28f660bc76ed6ce7572a18a2
ZheWana <zhewana@qq.com>
2023-08-01 10:36:50 +0800

    Debug

--- syncParser.py
+++ syncParser.py
@@ -4,12 +4,13 @@ res = feedparser.parse('https://zhewana.cn/?feed=atom')
 anchor = "<!-- Python Anchor -->"
 
 with open("./README.md", "w+") as f:
-    txt = f.read().split(anchor)[0] + anchor
-    f.seek(0)
+    # txt = f.read().split(anchor)[0] + anchor
+    for p in f.read().split(anchor):
+        print(p)
     # f.write(txt)
-    print(txt)
+    # print(txt)
 
-    f.write("|in Blog|\r\n| --- |")
-    for i,entry in enumerate(res.entries):
-        title = entry.title if entry.title != "" else "SmallTalk"
-        f.write("| " + entry.updated.split('T')[0] + ": [" + title + "]" + "(" + entry.link + ")" + " |")
+    # f.write("|in Blog|\r\n| --- |")
+    # for i,entry in enumerate(res.entries):
+    #     title = entry.title if entry.title != "" else "SmallTalk"
+    #     f.write("| " + entry.updated.split('T')[0] + ": [" + title + "]" + "(" + entry.link + ")" + " |")
