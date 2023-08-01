a69927e5e8d048ed1d3d7d95bc9cc8d782377a47
ZheWana <zhewana@qq.com>
2023-08-01 16:18:40 +0800

    try: 添加gitbook章节内容

--- syncProfile.py
+++ syncProfile.py
@@ -14,15 +14,19 @@ with open("./README.md", "r") as f:
     txt = f.read().split(anchor)[0] + anchor
 
 with open("./README.md", "w") as f:
-    f.write(txt + "\r\n")
+    f.write(txt + "\r\n" + "***\r\n")
 
-    f.write("|in Blog|\r\n| --- |" + "\r\n")
+    f.write("🎃Blog新鲜事: \r\n")
     for i,entry in enumerate(res.entries):
         title = entry.title if entry.title != "" else "SmallTalk"
-        f.write("| " + entry.updated.split('T')[0] + ": [" + title + "]" + "(" + entry.link + ")" + " |" + "\r\n")
+        f.write("* " + entry.updated.split('T')[0] + " - [" + title + "]" + "(" + entry.link + ")" + "\r\n")
+    
+    f.write("***\r\n📕Book新章节: \r\n")
 
 for repo in g.get_user().get_repos():
     print(repo.name)
     if repo.name == "BlogBook":
         for commit in repo.get_commits():
-            print("    " + commit.commit.message)
+            if commit != commit.split("chap: ")[0]:
+                with open("./README.md", "a") as f:
+                    f.write("* [" + commit.split("chap: ")[1] + "](doc.zhewana.cn)\r\n")
