74dffde745ae8ce84d9ca18a2b7ed5c0f22315c5
ZheWana <zhewana@qq.com>
2023-08-01 10:48:44 +0800

    添加换行

--- syncParser.py
+++ syncParser.py
@@ -12,9 +12,9 @@ with open("./README.md", "r") as f:
     txt = f.read().split(anchor)[0] + anchor
 
 with open("./README.md", "w") as f:
-    f.write(txt)
+    f.write(txt + "\r\n")
 
-    f.write("|in Blog|\r\n| --- |")
+    f.write("|in Blog|\r\n| --- |" + "\r\n")
     for i,entry in enumerate(res.entries):
         title = entry.title if entry.title != "" else "SmallTalk"
-        f.write("| " + entry.updated.split('T')[0] + ": [" + title + "]" + "(" + entry.link + ")" + " |")
+        f.write("| " + entry.updated.split('T')[0] + ": [" + title + "]" + "(" + entry.link + ")" + " |" + "\r\n")
