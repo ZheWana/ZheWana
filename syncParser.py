2f62081d817bca7cc368888ab2fe439f02b39fd3
ZheWana <zhewana@qq.com>
2023-08-01 10:46:22 +0800

    测试脚本

--- syncParser.py
+++ syncParser.py
@@ -1,16 +1,20 @@
-import feedparser
+import feedparser              
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 res = feedparser.parse('https://zhewana.cn/?feed=atom')
 anchor = "<!-- Python Anchor -->"
+global txt
 
-with open("./README.md", "w+") as f:
-    # txt = f.read().split(anchor)[0] + anchor
-    for p in f.read().split(anchor):
-        print(p)
-    # f.write(txt)
-    # print(txt)
+with open("./README.md", "r") as f:
+    txt = f.read().split(anchor)[0] + anchor
 
-    # f.write("|in Blog|\r\n| --- |")
-    # for i,entry in enumerate(res.entries):
-    #     title = entry.title if entry.title != "" else "SmallTalk"
-    #     f.write("| " + entry.updated.split('T')[0] + ": [" + title + "]" + "(" + entry.link + ")" + " |")
+with open("./README.md", "w") as f:
+    f.write(txt)
+
+    f.write("|in Blog|\r\n| --- |")
+    for i,entry in enumerate(res.entries):
+        title = entry.title if entry.title != "" else "SmallTalk"
+        f.write("| " + entry.updated.split('T')[0] + ": [" + title + "]" + "(" + entry.link + ")" + " |")
